03ba25f7de80e22960f2639bd229d93f03215d0a
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-25 16:35:44 +0300

    Выпонила Task47

--- семинар7/Task50/Program.cs
+++ семинар7/Task50/Program.cs
@@ -2,46 +2,47 @@
 //принимает позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание,
 // что такого элемента нет.
+
 Console.WriteLine("введите номер строки: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите номер столбца: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int [,] numbers = new int [5,5];
-FillArrayRandomNumbers(numbers);
+int [,] matrix = new int [5,5];
 
-if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+void InputMatrix(int[,] matrix)
 {
-    Console.WriteLine("такого элемента нет");
-}
-else
-{
-    Console.WriteLine($"значение элемента в строке {n} и в столбце {m} равно {numbers[n-1,m-1]}");
-}
-
-PrintArray(numbers);
-
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
         {        
-         for (int j = 0; j < array.GetLength(1); j++)
-            {
-               array [i,j] = new Random().Next(-10, 10)/10;
-            }   
+         for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix [i,j] =new Random().Next(0,10);
         }
 }
 
-void PrintArray(int[,] array)
+void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i,j] + " ");
-        }   
-        Console.Write("]");
-        Console.WriteLine(""); 
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j ]} \t");
+            Console.WriteLine(); 
     }
 }
+
+void ReleaseMatrix(int[,] matrix)
+{
+   
+     if (n > matrix.GetLength(0) || m > matrix.GetLength(1))
+    
+    Console.WriteLine("такого элемента нет");
+   
+    else
+   
+    Console.WriteLine($"значение элемента в строке {n} и в столбце {m} равно {matrix[n-1,m-1]}");
+   
+}
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
+PrintMatrix(matrix);
+
 
--- семинар7/task47/Program.cs
+++ семинар7/task47/Program.cs
@@ -2,15 +2,15 @@
 // заполненный случайными вещественными числами.
 void InputMatrix(double[,] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-matrix[i, j] = Math.Round(new Random().NextDouble()*10,2);
-Console.Write($"{matrix[i, j]} \t");
-}
-Console.WriteLine();
-}
+  for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+     for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        matrix[i, j] = Math.Round(new Random().NextDouble()*10,2);
+        Console.Write($"{matrix[i, j]} \t");
+      }
+        Console.WriteLine();
+      }
 }
 
 
@@ -21,3 +21,5 @@ Console.Write("Введите кол-во столбцов: ");
 int m = Convert.ToInt32(Console.ReadLine());
 double[,] matrix = new double[n,m];
 InputMatrix(matrix);
+
+
